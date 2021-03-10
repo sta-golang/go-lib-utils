fa67ee679b746e4f5e3d59e6666fb9e54c19308b
TheSevenSky <45283622+xy63237777@users.noreply.github.com>
2021-03-10 15:47:55 +0800

    Update stringset.go

--- algorithm/data_structure/set/stringset.go
+++ algorithm/data_structure/set/stringset.go
@@ -1,1 +1,58 @@
+package main
+const (
+	defCap = 16
+)
 
+type StringSet struct {
+	set map[string]struct{}
+}
+
+func NewHashSet(size ...int) *StringSet {
+	cap := 16
+	if len(size) > 0 {
+		if size[0] > 16 {
+			cap = size[0]
+		}
+	}
+	return &StringSet{set: make(map[string]struct{}, cap)}
+}
+
+
+func (hs *StringSet) Add(elements ...string) {
+	for i := range elements {
+		hs.set[elements[i]] = emptyElement
+	}
+}
+
+func (hs *StringSet) Remove(elements ...string) {
+	for i := range elements {
+		delete(hs.set, elements[i])
+	}
+}
+
+func (hs *StringSet) Contains(val string) bool {
+	if _, ok := hs.set[val]; ok {
+		return true
+	}
+	return false
+}
+
+func (hs *StringSet) Size() int {
+	return len(hs.set)
+}
+
+func (hs *StringSet) Empty() bool {
+	return hs.Size() == 0
+}
+
+func (hs *StringSet) Clear() {
+	hs.set = make(map[string]struct{}, defCap)
+}
+
+func (hs *StringSet) Iterator() []string {
+	ret := make([]string, 0, len(hs.set))
+	for key, _ := range hs.set {
+		ret = append(ret, key)
+	}
+	return ret
+}
